066445b9bd0a81265b1524565cf16c2df3c80b50
Avik Das <avikd957@gmail.com>
2026-08-22 11:08:43 +0530

    Step 22: update_outer_momentum

--- model.py
+++ model.py
@@ -375,8 +375,16 @@ def init_outer_optimizer(params):
         "momentum": momentum
     }
 
-# Step 22 - update_outer_momentum (not yet solved)
-# TODO: implement
+# Step 22 - update_outer_momentum
+def update_outer_momentum(outer_state, outer_grad, momentum_coef):
+    """Update Nesterov momentum buffer: m <- momentum_coef * m + outer_grad."""
+    for key in outer_state["momentum"]:
+        outer_state["momentum"][key] = (
+            momentum_coef * outer_state["momentum"][key]
+            + outer_grad[key]
+        )
+
+    return outer_state
 
 # Step 23 - nesterov_param_update (not yet solved)
 # TODO: implement
