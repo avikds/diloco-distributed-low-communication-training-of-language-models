61d6075fb9ed1e53c4b7a7c200095a2ed863e5ba
Avik Das <avikd957@gmail.com>
2026-08-22 10:49:34 +0530

    Step 10: adam_param_step

--- model.py
+++ model.py
@@ -178,8 +178,18 @@ def bias_correct_moments(state, beta1, beta2):
 
     return m_hat, v_hat
 
-# Step 10 - adam_param_step (not yet solved)
-# TODO: implement
+# Step 10 - adam_param_step
+def adam_param_step(params, m_hat, v_hat, lr, eps):
+    """Apply the core Adam adaptive parameter update without weight decay."""
+    new_params = {}
+
+    for key in params:
+        new_params[key] = (
+            params[key]
+            - lr * m_hat[key] / (np.sqrt(v_hat[key]) + eps)
+        )
+
+    return new_params
 
 # Step 11 - decoupled_weight_decay (not yet solved)
 # TODO: implement
